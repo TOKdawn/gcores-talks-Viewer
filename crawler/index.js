42a99abf0482106cf8bccdc0b47bcece2c1a43fa
Zhangpengyu <18746442232@163.com>
2022-04-08 11:42:14 +0800

    更新

--- crawler/index.js
+++ crawler/index.js
@@ -7,6 +7,9 @@ const cheerio = require('cheerio');
 const Gtalk = 'https://www.gcores.com/talks/'
 const GitUrl = 'https://github.com/TOKdawn/gcores-talks-Viewer/blob/main/crawler/TID.html'
 var  OLD_ID = ''
+const add=function(a,b){
+  return a+b
+}
 
 
 function setHTML(TID){
@@ -30,7 +33,6 @@ function getTID(){
     https.get(GitUrl, res => {
       let html = ''
       res.on('data', data => {
-        console.log('请求中:',data)
         html += data
       })
       res.on('end', () => {
@@ -48,35 +50,47 @@ function getTID(){
   })
 }
 
-function tryTID(TID){
-  let url = Gtalk + TID
-  return new Promise((resolve) => {
-    console.log('开始获取GTalk')
-    https.get(url, res => {
-      let html = ''
-      res.on('data', data => {
-        html += data
-      })
-      res.on('end', () => {
-        console.log('GTalk请求成功')
-        let $ = cheerio.load(html);
-        let DOM = $('.aat_container');
-        if(DOM){
-          resolve(1)
-        }else{
-          resolve(0)
-        }
+function tryTID(TID){//检测TID要同时连续监测三个,因为存在一些删除的机博,占用了TID但是页面拿取为空
+  let promiseList = []
+  for(let i = 0; i < 3; i++){
+    promiseList[i] = new Promise(resolve => {
+      let url = Gtalk + (TID+i)
+      https.get(url, res => {
+        let html = ''
+        res.on('data', data => {
+          html += data
+        })
+        res.on('end', () => {
+          console.log('GTalk请求成功',(TID+i))
+          let $ = cheerio.load(html);
+          let DOM = $('.aat_container');
+          if(DOM){
+            resolve(1)
+          }else{
+            resolve(0)
+          }
+        })
       })
     })
-  })
+  }
+  return promiseList
 }
+
 
 function donext(){
   console.log('其他逻辑')
 }
 async function run(){
-  OLD_ID = await getTID()
+  OLD_ID = await getTID() - 0
   console.log('获取TID成功:',OLD_ID)
+  
+  Promise.all( tryTID(OLD_ID + 100)).then(res => {
+    if(res.reduce(add) >= 1){  //三个中有一个有效就成功
+      console.log('检测TID结果:', '成功')
+    }else{
+      console.log('检测TID结果:', '失败')
+    }
+  })
   
 }
 
